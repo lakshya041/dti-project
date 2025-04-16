05cc905161857184ee2675e4acbcbc054a10e51b
Jashan32 <jashandeep1849@gmail.com>
2025-04-16 19:30:17 +0530

    added load all jobs route

--- backend/jobs/jobs.js
+++ backend/jobs/jobs.js
@@ -7,39 +7,67 @@ const JWT_SECRET = process.env.JWT_SECRET;
 import { z } from "zod";
 const addJobsRouter = Router()
 const removeJobsRouter = Router()
+const loadAllRouter = Router()
 import { employeeModel, jobsModel } from "../db";
+
+function generateRandomString() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+
+    for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters[randomIndex]; // Picks a random character from the string
+    }
+
+    return randomString;
+}
+
 
 addJobsRouter.post("/", async function (req, res) {
 
-
+    const jobId = generateRandomString()
     jobsModel.create({
-        username:req.username,
-        type: req.type,
-        location: req.location,
-        organization: req.organization,
-        description: req.description,
+        jobId: jobId,
+        username: req.body.username,
+        type: req.body.type,
+        location: req.body.location,
+        organization: req.body.organization,
+        description: req.body.description,
         salaryRange: {
-            min: req.minsalaryRange,
-            max: req.maxsalaryRange,
+            min: req.body.minsalaryRange,
+            max: req.body.maxsalaryRange,
         },
-        experienceLevel: req.experienceLevel,
+        experienceLevel: req.body.experienceLevel,
 
     })
 
     res.json({
-        message:"added success"
+        message: "added success"
     })
 
 })
 
 removeJobsRouter.post("/", async function (req, res) {
 
+    const jobId = req.jobId
     const username = jwt.verify(req.token, JWT_SECRET)
-    const nRes = await jobsModel.findOne({
-        username:username
+    await jobsModel.deleteOne({
+        username: username,
+        jobId: jobId
 
     })
 
+})
+
+loadAllRouter.get("/", async function (req, res) {
+    const username = req.headers.username
+    const jobs = await jobsModel.find({
+        username: username
+    })
+    res.json({
+        message: "got jobs",
+        jobs: jobs
+    })
 })
 
 export {
